50d18319b1e8b3c88eb6d4b73141c20705328286
Benjamin Liu <32715027+benliu961@users.noreply.github.com>
2024-12-02 17:05:11 +0000

    yuh

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -4,6 +4,20 @@ import java.util.*;
 
 public class QueryProcessor {
 
+    boolean isPrime(int n)
+    {
+        // Corner case
+        if (n <= 1)
+            return false;
+
+        // Check from 2 to n-1
+        for (int i = 2; i < n; i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
+    }
+
     public String process(String query) {
 
         if (query.toLowerCase().contains("shakespeare")) {
@@ -45,6 +59,49 @@ public class QueryProcessor {
             int first = Integer.parseInt(parts[2]);
             int second = Integer.parseInt(parts[5]);
             return Integer.toString(first * second);
+        }
+
+        if (query.contains("square")) {
+            String numbers = query.split(":")[1];
+            numbers = numbers.replaceAll("\\s+", "");
+            numbers = numbers.substring(0, numbers.length() - 1);
+            List<String> temp = Arrays.asList(numbers.split(","));
+            for (String n : temp) {
+                int a = Integer.parseInt(n);
+                int sqrt = (int) Math.sqrt(a);
+                boolean square = sqrt * sqrt == a;
+
+                int cbrt = (int) Math.cbrt(a);
+                boolean cube = cbrt * cbrt * cbrt == a;
+
+                if (square && cube) {
+                    return String.valueOf(a); 
+                }
+            }
+        }
+
+        if (query.contains("prime")) {
+            String numbers = query.split(":")[1];
+            numbers = numbers.replaceAll("\\s+", "");
+            numbers = numbers.substring(0, numbers.length() - 1);
+            List<String> temp = Arrays.asList(numbers.split(","));
+            List<String> tracker = new ArrayList<>();
+            for (String n : temp) {
+                int a = Integer.parseInt(n);
+                if (isPrime(a)) {
+                    tracker.add(String.valueOf(a));
+                }
+            }
+            return String.join(", ", tracker);
+        }
+
+        if (query.contains("minus")) {
+            // remove trailing ?
+            query = query.substring(0, query.length() - 1);
+            String[] parts = query.split(" ");
+            int first = Integer.parseInt(parts[2]);
+            int second = Integer.parseInt(parts[4]);
+            return Integer.toString(first - second);
         }
 
         return "";
